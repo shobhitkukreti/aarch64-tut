c6ec3a7ad4a8c87295b6df30cdd0fbfe64bf3854
shobhitkukreti <sk@beaglebone>
2016-01-25 17:25:12 -0800

    working SVC exception

--- mk.sh
+++ mk.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-aarch64-none-elf-gcc -c -g startup.S
+rm *.o 
+rm *.elf
+
+aarch64-none-elf-gcc -c -g  startup.S
 aarch64-none-elf-gcc -c -g main.c
 
 aarch64-none-elf-ld -g -T linker.ld startup.o main.o -o main.elf -nostdlib
